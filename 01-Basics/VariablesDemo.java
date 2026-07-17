0f1c2a35c702e874d37e224164cd146472c18c0c
Akshay Yadav <akshayryadav283@gmail.com>
2026-07-17 11:53:40 +0530

    Add variables and data types demo

---
+++ 01-Basics/VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        // Primitive data types
+        int age = 28;
+        double salary = 45000.50;
+        char grade = 'A';
+        boolean isEmployed = true;
+        String name = "Akshay";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Employed: " + isEmployed);
+    }
+}
